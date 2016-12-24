b2a8a408e15a3a3457b3adc8bc156f6d51178683
Renan Yuri Lino <renan.lino@gmail.com>
2016-12-24 21:45:40 -0200

    primeira versao do relogio analogico

--- Ex6/Ex6.ino
+++ Ex6/Ex6.ino
@@ -1,7 +1,7 @@
 /*
   www.CodeDecay.com.br
   Licença de uso: Apache 2.0
-  Neste exercício vamos acrescentar uma figura ao display.
+  Neste exercício vamos animar uma figura no display.
  */
 
 //Diz para este programa em que arquivo buscar as funções que serão utilizadas
@@ -20,34 +20,34 @@ int conta_frame = 0;
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C oled(U8G2_R0);  // Declara o nosso display OLED.
 
 void setup() {
-    
+
     Serial.begin(9600); // Inicializa comunicacao serial com o computador
     rtc.begin();        //  Inicializa comunicacao serial I2C com o RTC
     oled.begin();       // Inicializa a comunicacao com o display
 }
 
 void loop() {
-    
+
     // Adquire o horário atual do módulo RTC e armazena no objeto 'agora'
     DateTime agora = rtc.now();
 
     // Diz para o DS3231 realizar uma conversão de temperatura e deixá-la pronta para leitura. Sem este comando a temperatura é atualizada pelo DS3231 a cada 64 segundos.
     rtc.convertTemperature();
     // Lê o valor de temperatura através da comunicação I2C
-    float temp = rtc.getTemperature(); 
+    float temp = rtc.getTemperature();
 
     //Concatenação da string 'hora' no formato 'agora.hora():agora.minute():agora.second()'
     hora = String( agora.hour() ) + ":" + String( agora.minute() ) + ":" + String( agora.second() );
 
     //Aqui eh feita a mesma coisa, mas a temperatura eh transformada de float para int e de int para String e de string para vetor de caracteres na mesma linha
     temperatura = String( int(temp) ) + " C";
-    
-    
+
+
     //Executa os desenhos
     draw();
     //Envia os desenhos para o display
     oled.sendBuffer();
-    
+
     conta_frame = conta_frame + 1;
     if ( conta_frame > 1) conta_frame = 0;
 
@@ -61,7 +61,7 @@ void draw() {
     oled.setFont(u8g2_font_helvB12_tf); // Seleciona a fonte Helvetica Bold tamanho 12
     oled.setCursor(30, 50); // Posiciona o cursor de texto na coordenada (30,50)
     oled.print(temperatura); // Escreve a temperatura nas coordenadas (30,50) do display
-    
+
     if (conta_frame) oled.drawXBMP(128-pusheen_width, 0, pusheen_width, pusheen_height, pusheen_bits); // Desenha o gatinho olhando para a esquerda se conta_frame == 1
     else oled.drawXBMP(128-pusheen_flip_width, 0, pusheen_flip_width, pusheen_flip_height, pusheen_flip_bits); // Desenha o gatinho olhando para a direita se conta_frame == 0
 }
---
+++ Ex7/Ex7.ino
@@ -0,0 +1,103 @@
+/*
+  www.CodeDecay.com.br
+  Licença de uso: Apache 2.0
+  Neste exercício vamos substituir o relógio digital por um relógio analógico.
+ */
+
+//Diz para este programa em que arquivo buscar as funções que serão utilizadas
+#include <Sodaq_DS3231.h>
+#include <U8g2lib.h>
+#include <U8x8lib.h>
+#include <math.h> //para as operações com ângulos
+
+// Arquivos das figuras
+#include "pusheen.h"
+#include "pusheen_flip.h"
+
+String hora; // Esta string (uma sequencia especial de caracteres) sera utilizada para armazenar nosso horario
+String temperatura; // Esta string (uma sequencia especial de caracteres) sera utilizada para armazenar a temperatura
+int conta_frame = 0;
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C oled(U8G2_R0);  // Declara o nosso display OLED.
+
+void setup() {
+
+    Serial.begin(9600); // Inicializa comunicacao serial com o computador
+    rtc.begin();        //  Inicializa comunicacao serial I2C com o RTC
+    oled.begin();       // Inicializa a comunicacao com o display
+}
+
+void loop() {
+
+
+    //Executa os desenhos
+    draw();
+    //Envia os desenhos para o display
+    oled.sendBuffer();
+
+    conta_frame = conta_frame + 1;
+    if ( conta_frame > 1) conta_frame = 0;
+
+    delay(500);
+
+}
+
+void draw() {
+    double passoHoras, passoMin, passoSeg, posicao;
+    int x0, y0, raio;
+
+    /*
+    Vamos calcular o passo, em radianos, que cada ponteiro anda.
+      - O ponteiro das horas anda 2.pi radianos (360 graus)
+    para cada 12 horas, portanto, 2.pi/12 radianos por hora
+      - Os ponteiros dos minutos anda 2.pi radianos a cada 60 minutos,
+    portanto, 2.pi/60 radianos por minuto.
+      - O ponteiro dos segundos anda 2.pi radianos a cada 60 segundos,
+    portanto, 2.pi/60 radianos por segundo (mesmo valor que o passo dos
+    minutos).
+    */
+
+    passoHoras = 2*PI/12.0;
+    passoMin = 2*PI/60.0;
+    passoSeg = passoMin;
+
+    oled.clearBuffer(); // Limpa a tela de desenho
+
+    DateTime agora = rtc.now();
+
+    // Circunferecia de raio = 30 e centro em (x0 = 32, y0 = 32)
+    x0 = 32;
+    y0 = 32;
+    raio = 30;
+    oled.drawCircle(x0, y0, raio);
+
+    // Ponteiro das horas
+    // A posicao atual do ponteiro em radianos é: hora atual * passoHoras
+    // Desenha uma linha que parte de (x0, y0) e vai até
+    // (x0 + raio*cos(posição do ponteiro), y0 - raio*sen(posição do ponteiro))
+    posicao = agora.hour()*passoHoras;
+    raio = 13;
+    oled.drawLine( x0, y0, x0 + raio*sin(posicao), y0 - raio*cos(posicao) );
+
+    // Ponteiro dos minutos
+    // A posicao atual do ponteiro em radianos é: minuto atual * passoMin
+    // Desenha uma linha que parte de (x0, y0) e vai até
+    // (x0 + raio*cos(posição do ponteiro), y0 - raio*sen(posição do ponteiro))
+    posicao = agora.minute()*passoMin;
+    raio = 20;
+    oled.drawLine( x0, y0, x0 + raio*sin(posicao), y0 - raio*cos(posicao) );
+
+    // Ponteiro dos segundos
+    // A posicao atual do ponteiro em radianos é: minuto atual * passoSeg
+    // Desenha uma linha que parte de (x0, y0) e vai até
+    // (x0 + raio*cos(posição do ponteiro), y0 - raio*sen(posição do ponteiro))
+    posicao = agora.second()*passoSeg;
+    raio = 20;
+    oled.drawLine( x0, y0, x0 + raio*sin(posicao), y0 - raio*cos(posicao) );
+
+    oled.setFont(u8g2_font_helvB14_tf); // Seleciona a fonte Helvetica Bold tamanho 14
+    oled.setCursor(75, 32-14); // Posiciona o cursor de texto na coordenada (10,20)
+    oled.print( String( agora.hour() ) + "h" + String( agora.minute() ) );
+
+
+}
